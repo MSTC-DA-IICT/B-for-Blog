3c0c6692f2810b084f3ab0cd033839ce0d2ede57
kamalrt11 <54072216+kamalrt11@users.noreply.github.com>
2021-10-05 09:58:52 +0530

    Update settings.py

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

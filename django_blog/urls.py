8c4898880112c6b56cd86b94323a3cc351d59ca0
Dishant <keenlearner13579@gmail.com>
2021-10-06 20:43:44 +0530

    Updated django_blog/urls.py

--- django_blog/urls.py
+++ django_blog/urls.py
@@ -14,9 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
-from blog import views
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include("blog.urls")),
